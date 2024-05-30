da6db8ae7657392f49f5c3af28ae3958372c8849
bugg-m <mkchauhan0543@gmail.com>
2024-05-30 22:36:53 +0530

    [PLO] BE-01 Initial project setup

--- src/root.app.ts
+++ src/root.app.ts
@@ -1,13 +1,39 @@
-import express, { Request, Response, Application } from 'express'
-import dotenv from 'dotenv'
+import express, { Application, urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+const app: Application = express();
 
 //For env File
-dotenv.config()
+dotenv.config({
+    path: `${__dirname}/../.env`
+});
 
-const APP: Application = express()
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL1,
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        exposedHeaders: ["Authorization"],
+        optionsSuccessStatus: 200
+    })
+);
 
-APP.get('/', (req: Request, res: Response) => {
-    res.send('Welcome bugg!')
-})
+app.use(express.json({ limit: "16kb" }));
+app.use(urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-export default APP
+// Route imports
+import { UserRouter } from "@routes/user-routes/user.routes";
+import { USER_API_ROUTE } from "app.constants";
+
+// ============================== Mini React Apps Routes ==================================
+
+// passkeys app
+app.use(USER_API_ROUTE, UserRouter);
+
+// ============================== Mini React Apps Routes ==================================
+export { app };
